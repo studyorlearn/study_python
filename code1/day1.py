27303e8b7757e791cec2ecfdb5be6db9bb770ecc
studyorlearn <boychen2022@163.com>
2022-04-19 03:18:45 +0800

    Create day1.py

---
+++ code1/day1.py
@@ -0,0 +1,11 @@
+#  \转义字符
+print("hellow world")
+print("hellow\nworld")
+print("hellow\tworld")
+print("helloooo\tworld")
+print("hellow\rworld")
+print("hellow\bworld")#back a letter
+print("http:\\\www.baidu.com")
+#原字符 不希望转义字符\起作用  结尾不能是\ 
+print(r'hellow\world')
+print(r'hellow\world\2')
